78e5532c70eb88987164d69bca6a1b8639697101
Katie Duane <katiejduane@gmail.com>
2019-01-02 12:22:03 -0500

    initial commit

---
+++ research_data/georgiaFarmData.js
@@ -0,0 +1,82 @@
+//Georgia Farm Info
+
+let cropInfo = [
+    {
+        crop: "Peanuts",
+        historicalFact: "",
+        revenue: "",
+        jobs: "", 
+        recipe: "",
+        famousFarmer: "Jimmy Carter"
+    
+    },
+
+    {
+        crop: "Peaches"
+    },
+
+    {
+        crop: "Blueberries"
+    },
+
+    {
+        crop: "Cotton"
+    },
+
+    { 
+        crop: "Sweet Potatoes"
+    },
+
+    {
+        crop: "Watermelon"
+    }
+]
+
+
+let critterInfo = [
+    {
+        critter: "Hogs"
+
+    },
+
+    {
+        critter: "Chickens",
+        produce: "eggs and poultry",
+    },
+
+    {
+        critter: "Cows",
+        produce: "beef and milk",
+    }
+
+]
+
+
+let treeInfo = [
+    {
+        tree: "Peach Tree"
+    },
+    {
+        tree: "Pecan Tree"
+    },
+    {
+        tree: "Southern Live Oak"
+    }
+]
+
+
+
+let farmInfo = [
+    {
+        farmName: "",
+        blurb: "",
+        latLong: "",
+    }
+]
+
+console.log(cropInfo[0].crop)
+//include moonshine!? but where!?
+
+let apisToMaybeUse = [
+    googleMaps,
+]
