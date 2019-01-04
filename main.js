c965db8f64d3b3e502ebba5c3fbed0d402b5aa6b
jasonpbenson <jasonbenson3rd@gmail.com>
2019-01-04 13:06:35 -0500

    updated css and html to better facilitate flexbox styling

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 $(document).ready(()=>{
-    console.log ("sanity check")
-    const queryForm = document.querySelector('.google-query-form')
-    let mapPic = ``
-    console.log(queryForm)
+    // console.log ("sanity check")
+    // const queryForm = document.querySelector('.google-query-form')
+    // let mapPic = ``
+    // console.log(queryForm)
     $('.google-query-form').submit((event)=>{
         event.preventDefault()
-        const symbol = encodeURI($('#symbol').val())
-            $('.submit-button').append(`
-            <iframe width="600" height="450" frameborder="0" style="border:0" src="https:/
-            /www.google.com/maps/embed/v1/search?q=${symbol}&key=${apiKey}" allowfullscreen></iframe>
+        // const symbol = encodeURI($('#symbol').val())
+            $('.compass').html(`
+            <iframe width="200" height="200" frameborder="0" style="border:0" 
+            src="https://www.google.com/maps/embed/v1/search?q=farms%20within%2015%20miles%20of%20my%20location&key=${apiKey}" 
+            allowfullscreen></iframe>
         `)
     })// end query form
 })//end ready form
