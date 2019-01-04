c9f609e4bc5963e303d8da2026f087d535836828
Jim Owens <jimcowens@gmail.com>
2019-01-04 12:11:45 -0500

    added info

--- research_data/georgiaFarmData.js
+++ research_data/georgiaFarmData.js
@@ -54,48 +54,58 @@ let cropInfo = [
 let critterInfo = [
     {
         critter: "Hogs",
-        produce: "",
-        historicalFact: "",
-        medicinal: "",
-        history: "",
-        myth: "",
-        symbolism: "",
-        georgia: "",
-        weirdFact: "The words 'pig', 'hog' and 'swine' are used interchangeabley.",
-        funFact: "Their intelligence level is inferior only to some apes, dolphins, and elephants."
+        latinName: `sus scrofa (domesticus) or sus indicus`,
+        produce: "Although listed as unclean by some people-groups, Pork is the most widely consumed meat in the world. Bacon, sausage, pork chops, etc... are staples. Still others fit more esoteric ends: pork feet, chitterlings, and pig ears. Their sense of smell is superior to almost every animal as well. Pork skin is used for leather(football or drum heads), guts for violin strings, bones for china, oil for cutting/industrial lubricants, gelatin for marshmallows... the list is nearly endless. Tattoo artists use their skin for practice and doctors use their organs for preclinical study because they are so close to those of humans.",
+        historicalFact: "Pigs, like humans, are omnivores. While mankind domesticated the beast, they are the only species known that can revert to a feral animal in under one generation. When left alone and uncared for, their hide regrows, teeth grow to tusks, and disposition becomes far less amenable.",
+        medicinal: "Pig insulin can be used for diabetes treatment, gelatin for pills, porcine burn dressings, even their hearts can be used for xenotransplantation.",
+        history: "The domesticated pig is from the common Eurasian wild boar. Related to the peccary, Babirusa, and warthog, the pig first became its modern form ~500,000 year ago, and was domesticated ~7,000 B.C. They were first introduced to Europe in the 18th-19th centuries. There are many subspecies prized for various traits, like sense of smell, size, or taste. They have been used mostly for food, but also for leather, hunting (truffles, mostly), and even companionship. ",
+        myth: "In their Asian origin, the year of the pig is important. They historically represent a beautiful personality and are blessed with good fortune in life.",
+        symbolism: "In Yin and Yang, the pig is the yin. In Asian cultures, they symbolize wealth. In Western cultures, they symbolize excess, laziness, awkwardness, and obesity.",
+        georgia: "Feral pigs in Georgia, especially the south, are wreaking havoc in local farms. Domesticated pigs are farmed as typical (inhumanely, mostly) to other states.",
+        weirdFact: "Pigs can tell when they have done something a person likes, and can duplicate it. They also can make enjoyable pets.",
+        funFact: "Their intelligence level is inferior only to some apes, dolphins, and elephants. They can play certain concentration games with higher success and focus than a chimpanzee."
 
     },
 
     {
         critter: "Chickens",
+        latinName: `gallus gallus (domesticus)`,
         produce: "Eggs and Poultry(Broilers)",
-        historicalFact: "",
-        medicinal: "",
-        history: "",
-        myth: "",
-        symbolism: "",
-        georgia: ""
+        historicalFact: "Chickens were used solely for their eggs until about 150-200 years ago. They were unclean and difficult to eat without causing sickness, so they were only eaten by the wealthy who could afford to have them prepped correctly. In modern times, chicken is a staple and affordable food, especially in quick-service dining.",
+        medicinal: "Chicken meat has a high protein/fat ratio, which is helpful in weight management and bodybuilding. Eggs are low in sugar and high in protein as well. They are very healthy alternatives to other foods, but it's worth noting also high in cholesterol.",
+        history: "Descended fom the Red Junglefowl, Chickens were first domesticated around 3,000 B.C. they helped keep pests (grubs, bugs, beetles, worms, etc...) out of owner's crops and fields, and their excrement was good for fertilizer.",
+        myth: "Chickens are used in some (predominantly Voodoo or Voodoo derivatives) religions for ritual sacrifice. Fortune-tellers have also read their entrails for milennia. They were the sacred bird of the Goddess Athens, the Goddess of wisdom and warfare. Chickens were so sacred in Ancient Greece that they were kept for eggs and religious purposes only.",
+        symbolism: "In Roman times, chickens were related to the Sun God, and their clucking was related to Eros (erotic love). They were a symbol of commerce and productivity as well.",
+        georgia: "Many homes in Georgia raise chickens independently, but they are also farmed in large numbers for large scale consumption. Different breeds are used for meat or eggs.",
+        weirdFact: `Chickens have been used for thousands of years for man's entertaiment in cockfighting. Male chickens, or cocks, have spurs on their legs and can be very aggressive.`,
+        funFact: `The humble chicken is arguably the closest living relative to the Tyrannosaurus Rex.`,
     },
 
     {
         critter: "Cows",
+        latinName: ``,
         produce: "Beef and Milk",
         historicalFact: "",
         medicinal: "",
         history: "",
         myth: "",
         symbolism: "",
-        georgia: ""
+        georgia: "",
+        weirdFact: ``,
+        funFact: ``,
     },
 
     {
         critter: "Goat",
+        latinName: ``,
         historicalFact: "",
         medicinal: "",
         history: "",
         myth: "",
         symbolism: "",
-        georgia: ""
+        georgia: "",
+        weirdFact: ``,
+        funFact: ``,
 
     }
 
