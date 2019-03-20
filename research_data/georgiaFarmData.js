e7031158bbdf46350a5c4a901cd41f35b3f8c127
jasonpbenson <jasonbenson3rd@gmail.com>
2019-03-20 12:07:29 -0400

    replaced farm data that was oddly missing

---
+++ research_data/georgiaFarmData.js
@@ -0,0 +1,166 @@
+    //plants
+    let cropInfo = [
+        {
+            crop: "Blueberries",
+            nativeTo: "Origin: North America,",
+            latin: "Latin: Vaccinium corymbosum",
+            historicalFact: "Blueberries are one of only three berries native to North America. The continent’s earliest inhabitants were first to use the tiny berries, both fresh and dried, for their flavor, nutrition, and healing qualities.",
+            medicinal: "Blueberries have the highest antioxidant capacity of all the popular fruits and vegetables. ",
+            myth: "Native North Americans believed the Wild Blueberry had magical powers. Atop each berry is the base of its earlier flower, a calyx in the shape of a five-pointed star. Legend has it that during a time of starvation, the Great Spirit sent these “star berries” down from the heavens to relieve the hunger of his children.",
+            georgia: "The blueberry industry is concentrated in the flatwoods of southeast Georgia. In 2014, Georgia produced more bueberries than any other state."
+        },
+
+        {
+            crop: "Peaches",
+            nativeTo: "Origin: Northwest China",
+            latin: "Latin: Prunus persica",
+            historicalFact: "The name 'persica' refers to its widespread cultivation in Persia (modern-day Iran), from where it was transplanted to Europe. One of the old meanings of Tao is 'peach'.",
+            medicinal: "Peaches are a great source of zinc, which also has anti-aging properties. Eating peaches also may decrease neurodegenerative risk of disorders, such as Alzheimer's disease.",
+            myth: "In Chinese mythology, the 'Peaches of Immortality' are consumed by people to make them immortal. The peach was also a standard symbol of female genitalia and was connected with Taoist sexual mysticism.",
+            georgia: "Georgia ranks third, in the US, for peach production, but China wins for the world.",
+
+        },
+
+        {
+            crop: "Honey",
+            nativeTo: "Origin: Anywhere that has flowers or trees",
+            latin: "Latin: apis mellifera (though, that's the honey bee)",
+            historicalFact: "Humans have been cultivating honey since ancient times. A cave painting of a honey-seeker in Valencia Spain dates back 8,000 years.",
+            medicinal: "Honey can be used to treat mild burns, chronic and acute coughs, and can also be useful for controlling the side effects of radiation and chemotherapy.",
+            myth: "In Greek mythology, bees were considered messengers of the gods and honey to be a source of wisdom and poetry. The gods of Olympus lived on honey (nectar) and honey wine (ambrosia). In Greek, the word 'nectar' means 'victory over death'. ",
+            georgia: "Decatur is Georgia's first officially 'Bee-Friendly city. Georgia also has a State Prison Beekeeping Program. Types of honey commonly sold in Georgia are Orange Blossom, Sourwood, Tupelo, Tulip Poplar, Clover, Gallberry, and Wildflower Honey."
+        },
+
+        {
+            crop: "Collard Greens",
+            nativeTo: "Origin: Greece",
+            latin: "Latin: brassica oleracea",
+            historicalFact: "They've been eaten for at least 2000 years, with evidence pointing to Ancient Greeks cultivating several varities.",
+            medicinal: "Collard greens are tastier and more nutritious when cultivated in the colder months. They've have been associated with cancer prevention, detoxification, anti-inflammatory properties, heart health, and digestive support.",
+            myth: "Eating collard greens on New Years Day is said to bring one good luck.",
+            georgia: "A popular cultivar of collard greens includes the 'Georgia Southern'."
+
+        },
+    ]
+
+    //animals
+    let critterInfo = [
+        {
+            critter: "Hogs",
+            latin: "Latin: sus scrofa (domesticus) or sus indicus.",
+            fact1: "Tattoo artists use their skin for practice and doctors use their organs for preclinical study.",
+            fact2: "Pigs can return to a feral state in less than one generation; when left alone and uncared for, their hide regrows, teeth grow into tusks, and disposition becomes far less amenable.",
+            myth: "Pigs were holy in Irish and Germanic mythology. In Norse Mythology,'Gullinbursti' was a pig made by two dwarves using thousands of pieces of gold wire. He shone like the sun and could speed through the sky, making plants grow. He also used his tusks to rake the earth and show men how to plow the land.",
+            symbolism: "In Yin and Yang, the pig is the yin; soft, cool, and yielding. In Asian cultures, they symbolize wealth. In Western cultures, they symbolize excess, laziness, awkwardness, and obesity.",
+        
+        },
+
+        {
+            critter: "Cattle",
+            latin: "Latin: Bos taurus",
+            fact1: "Calves can discriminate between humans based on previous experience; they'll approach those who handled them positively and avoid those who handled them aversively.",
+            fact2: "Cattle are very gregarious and even short-term isolation is considered to cause severe psychological stress; mirrors are sometimes used to calm cows that are isolated.",
+            myth: "The cow-headed Egyption goddess Hathor was originally a personification of the Milky Way, which was considered to be the milk that flowed from the udders of a heavenly cow.",
+            symbolism: "Cattle are considered sacred in world religions such as Hinduism, Jainism, and Buddhism, symbolizing patience, abundance, gentleness, and holiness.",
+        
+        },
+
+        {
+            critter: "Chickens",
+            latin: "Latin: gallus gallus (domesticus)",
+            fact1: "The humble chicken is arguably the closest living relative to the Tyrannosaurus Rex.",
+            fact2: "While Chickens have been domesticated for 5,000 years, they descended fom the Red Junglefowl in southeast Asia. The male Junglefowl's tail is composed of long, arching feathers that initially look black, but shimmer with blue, purple, and green in bright light.",
+            myth: "They are used in some (predominantly Voodoo) religions for ritual sacrifice. Fortune-tellers have also read their entrails for milennia. They were the sacred bird of the Goddess Athena.",
+            symbolism: "In Roman times, chickens were related to the Sun God, and their clucking was related to Eros. They were also symbolic of commerce and productivity. The chicken figures as a symbol of parental and spiritual love in the literature of the West. ",
+
+        },
+
+        {
+            critter: "Goat",
+            latin: "Latin: capra aegagrus hircus",
+            fact1: "Goats are naturally curious, exploring anything new or unfamiliar in their surroundings. They are also agile and known for their ability to balance in precarious places; they frequently climb trees.",
+            fact2: "Similar to dogs, goats will look to a human for assistance when faced with a challenge that they need help with.",
+            myth: "Few other domestic animals have suffered as much sacrifical killing as the goat. Popular Christian folk tradition in Europe associated Satan with imagery of goats.",
+            symbolism: "The faun is a mythological half human–half goat creature appearing in Ancient Rome. They were symbolic of fertility, and were said to inspire fear in men traveling in lonely, remote, or wild places.",
+        
+        }
+    ]
+
+    let aboutInfo = [
+        {
+            name: "Katie",
+            meaning: "The name Kathryn is: pure, clear. From the Latin Katharina",
+            origin: "Katie Duane is originally from Western New York, but she's also lived in Seattle, Portland, Boston, and Guayaquil (Ecuador). She writes essays and poems, and also code for the web. ",
+            link: `<a href ="https://github.com/katiejduane" target="_blank">Github Profile</a>`
+        },
+        {
+            name: "Jason",
+            meaning: "The name Jason: healing. The name was borne in Greek mythology. Var: Jayson.", 
+            origin: "Jason Benson is originally from Baltimore, Maryland. He has also lived in Chicago and San Francisco. He makes paintings, sculptures, mosaics, and now websites.",
+            link: `<a href ="https://github.com/jasonpbenson" target="_blank">Github Profile</a>`,
+        },
+        {
+            name: "Jeremy",
+            meaning: "From the given name Jeremiah. The weeping prophet.",
+            origin: "Jim Owens is originally from Albany, Georgia, but was raised in Rome (Georgia). He's super into math, has managed a few restaurants, and currently works as a web developer.",
+            link: `<a href ="https://github.com/jimboowens" target="_blank">Github Profile</a>`,
+        },
+        {
+            name: "Pithos",
+            meaning: "The Greek name for a container or vessel. A farm is a kind of container. So is a website.",
+            origin: "We wanted to create a space that would allow the user to experience an 'ordinary' thing in an unusual way. We were interested in ecosystems, and wanted to connect our ecosystem to the state of Georgia (home), and also the larger realms of history, mythology, and health; which are their own kind of ecosystems, or containers. <i>Pithos</i>.",
+            link: "",
+        }
+    ]
+
+let farmInfo = [
+
+    {
+        farmName: "Aluma Farm",
+        blurb: `<a href ="https://www.alumafarm.com/Aluma" target="_blank">Aluma Farm</a>` + ": Breaking ground along the Westside Trail of the Atlanta BeltLine, two farmers strive to transform an old industrial lot into a thriving farm in Adair Park.",
+    },
+    {
+        farmName: "Tucker Farms",
+        blurb: `<a href ="https://tuckerfarmsga.com/" target="_blank">Aluma Farm</a>` + " is a predominantly restaurant-supported farm located in Rome, Georgia. We grow specialty lettuces, greens, and herbs as well as seasonal garden vegetables.",
+    },
+    {
+        farmName: "Truly Living Well",
+        blurb: `<a href ="https://www.trulylivingwell.com/" target="_blank">Truly Living Well Farm</a>` + " is an urban farm providing natural and organic produce and farmer training to local Atlanta community. Programs for adults and youth.",
+    },
+    {
+        farmName: "Doghead Farms",
+        blurb: `<a href ="https://www.concrete-jungle.org/programs/doghead-farm/" target="_blank">Doghead Farms</a>` + ": Also known as Concrete Jungle, DogHead Farms also grows vegetables to donate in Southwest Atlanta. This allows hosting various things, including the Food Bank of Northeast Georgia.",
+    },
+    {
+        farmName: "Country Gardens Farm",
+        blurb: `<a href ="http://countrygardensfarm.com/" target="_blank">Country Gardens Farm</a>` + " is a multi-generational family farm, located in Coweta County, Georgia, which grows local farm fresh food. All of their products are grown without chemicals, antibiotics, or hormones.",
+    },
+    {
+        farmName: "Hunter Cattle Company",
+        blurb: `<a href ="http://huntercattle.com/" target="_blank">Hunter Cattle</a>` + " (Bold but tender) is a family owned business that raises free range, antibiotic-, hormone-, and steroid-free 100% Grassfed Beef and Pasture Raised Pork. This creates products providing great nutrition and incredible flavor.",
+    },
+    {
+        farmName: "White Oak Pastures",
+        blurb: `<a href ="https://www.whiteoakpastures.com/" target="_blank">White Oak Pastures</a>` + " is a 152 year-old family farm in Georgia, who takes pride in farming practices focused on regenerative land management, humane animal husbandry, and revitalization of the rural community.",
+    },
+    {
+        farmName: "Heritage Farm Georgia",
+        blurb: `<a href ="http://heritage-farm.net/" target="_blank">Heritage Farm</a>` + " is a pasture based, family farm located in Georgia, whose mission is to produce high quality, great tasting, nutritious foods and educate others on the importance of local, sustainable farms.",
+    },
+    {
+        farmName: "Mountain Valley Farm",
+        blurb: `<a href ="http://grassfedgeorgia.com/" target="_blank">Mountain Valley Farm</a>` + " is proud to offer Grass-Fed Beef wish no hormones, antibiotics, steroids, pesticides, or chemical fertilizers raised on pasture in a stress free environment & handled humanely throughout all life stages.",
+    },
+    {
+        farmName: "Love is Love Farm at Gaia Gardens",
+        blurb: `<a href ="http://www.loveislovefarm.com/" target="_blank">Love is Love Farm at Gaia Gardens</a>` + ": Beginning in 2008, Love is Love desires to serve the good food movement through mindful land stewardship. The Glover Family Farm hosts Love is Love Farm in Douglasville and recently moved to Gaia Gardens in Decatur back in 2011.",
+    },
+    {
+        farmName: "Rise 'N Shine Organic Farm",
+        blurb: `<a href ="https://risenshineorganicfarm.com/" target="_blank">Rise-n-Shine Organic Farm</a>` + " has grown into one of the largest Community Supported Agriculture (CSA) farms in Georgia supplying a bounty of certified organic produce to Metro Atlanta and Northwest Georgia.",
+    },
+    {
+        farmName: "HoneyWood Farms",
+        blurb: `<a href ="http://www.honeywoodfarms.com/" target="_blank">Honeywood Farms</a>` + " is a diversified livestock and market garden operation that practices holistic sustainable farming practices rooted in deep ecological principals.",
+    },
+]
